082a4b039b56d1e353af3871f98612ef5a31bed6
DavidAtikpo <davidatikpo44@gmail.com>
2025-10-13 10:06:53 +0000

    mis a jour

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function HomePage() {
         <ShippingBanner />
         <Header />
         <main className="flex-1">
-          <HeroSection />
+          {/* <HeroSection /> */}
           <ProductGrid />
         </main>
         <SubscribeFollow />
